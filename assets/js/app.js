1e19eac9618afb29c5c493d71567245178dc29c5
AkhalkatsiAvtandil <akhalkatsiavto@gmail.com>
2026-05-28 23:23:33 +0400

    personal_number-ის გამართვა კონტენტის მიხედვით ჯავასკრიპტიდან

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,36 @@
 const personalnumberinput = document.querySelector("#personal_number");
+const mobilenumberinput = document.querySelector("#mobile_number");
+const loginForm = document.querySelector("#login-form");
+const personalnumberPattern = /^\d{11}$/;
+// personalnumberinput.addEventListener("input", (e) => {
+//   console.log(e.target.value);
+// });
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(
+    "Form submitted",
+    personalnumberinput.value,
+    mobilenumberinput.value,
+  );
+  const personalnumberValue = personalnumberinput.value;
+  const mobilenumbeValue = mobilenumberinput.value;
 
-personalnumberinput.addEventListener("input", (e) => {
-  console.log(e.target.value);
+  //   const personalnumberValue = /^\d{11}$/.test(input);
+
+  if (personalnumberValue.trim() === "") {
+    personalnumberinput.closest(".input-group").classList.add("error");
+    personalnumberinput
+      .closest(".input-group")
+      .querySelector(".message").textContent = "Personal numberi is required!";
+  } else if (!personalnumberPattern.test(personalnumberValue)) {
+    personalnumberinput.closest(".input-group").classList.add("error");
+    personalnumberinput
+      .closest(".input-group")
+      .querySelector(".message").textContent = "Must contain 11 digits!";
+  } else {
+    personalnumberinput.closest(".input-group").classList.add("error");
+    personalnumberinput
+      .closest(".input-group")
+      .querySelector(".message").textContent = "";
+  }
 });
